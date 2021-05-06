c57b49a24f83d6d69a4539a9665fffa6b023b4b5
Nirnav-web <76113884+Nirnav-web@users.noreply.github.com>
2021-05-06 19:47:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+    width: 310,height: 300,
+    image_format: 'png',
+    png_quality: 100,
+    constraints:{
+        facingMode:"environment"
+    }
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+ function take_snapshot(){
+     Webcam.snap(function(data_uri){
+         document.getElementById("result").innerHTML="<img id='captured_img' src='>"+data_uri+"'>";
+     })
+ }
+ console.log("ml5.version",ml5.version);
+ classifier=ml5.imageClassifier('MobileNet',modelLoaded);
+ function modelLoaded(){
+     console.log("model.loaded");
+ }
+ function check(){
+     img=document.getElementById("captured_img");
+     classifier.classify(img,gotResult())
+ }
+function gotResult(error,result){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(result);
+        document.getElementById("object_name").innerHTML=result[0].label;
+    }
+}
